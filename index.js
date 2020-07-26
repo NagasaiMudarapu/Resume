b4dfe824467b7183ffde7c613848dc2adb1cddca
NagasaiMudarapu <nagasaimudarapu74@gmail.com>
2020-07-26 14:40:04 +0530

    Resume

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var ep = require("express");
 var ap = ep();
+ap.use(ep.static(__dirname + '/Frontend'));
 ap.get('/', function(req,res){
     res.sendFile(__dirname + '/Frontend/HTML/index.html');
 });
 ap.get('/resume', function(req,res){
     res.sendFile(__dirname + '/Frontend/HTML/resume.html');
 });
+ap.get('/mail', function(req,res){
+    res.send("Mail Service Will be Added soon!!");
+});
+
 var po = process.env.PORT || 3000;
 ap.listen(po,function(){
     console.log("Site Running on http://localhost:" + po);
